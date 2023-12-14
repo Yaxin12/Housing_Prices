e79a434c3e2792a2e02569780fb0ae42c0a68eb4
PBharati99 <bpeddinti717@gmail.com>
2023-12-14 17:28:26 -0500

    finalizing

--- project.py
+++ project.py
@@ -74,10 +74,10 @@ if selected=="Intro":
 
             The app has three pages:
 
-            -**Home**:    Introduction
-            -**Search**:  It allows user to search different locations through zip codes and cities
+            -_**Home**:    Introduction
+            -_**Search**:  It allows user to search different locations through zip codes and cities
                          and can compare them with different metrics or explore them.
-            -**Analyze**: Getting insights of house prices through boxplot measures.
+            -_**Analyze**: Getting insights of house prices through boxplot measures.
 
             """
             )
@@ -86,6 +86,7 @@ if selected=="Intro":
         st.markdown(
             """
             **Data Sources and Preparation**
+
             The data used in this app is found in different locations. 
             1. Demographic, housing at zip level (https://data.census.gov/)
             2. Environmental metrics (https://data.cdc.gov/)
@@ -99,20 +100,24 @@ if selected=="Intro":
         st.markdown(
             """
             **Use Case**
+
             It is important to note why, where, and how the property rates are showcasing the growth in different parts of United States.
             This can enable users like buyers, dealerships, and people companies target specific cities/counties depending on prices and location.
 
             **App Features**
+
             - Analyze which cities and states have better housing rates depending on various metrics.
             - View and compare the cities to analyze the similarity scores and choose the desired living location.
             - Analyze the annual appreciation of residential properties to determine the housing rates of 50 states from United States. 
               View data in different quartiles for better insights.
 
             **Future Work**
+
             This app can be extended to include additional countries and add more living location metrics to analyze the 
             best city/state for buying the properties.
 
             **Streamlit App**
+            
             https://housingprices.streamlit.app/
             """
             )
